a0ebcb928c313b00337b5dd9f26ce1312f27ee9a
yeounoh <yeounohster@gmail.com>
2018-04-11 23:24:04 -0400

    optimizer bug fix

--- util/custom_lib.py
+++ util/custom_lib.py
@@ -9,8 +9,26 @@
 """
 import pandas as pd
 import numpy as np
+from scipy.stats import randint, uniform
+from time import time
+import concurrent.futures
+import sys, os
+if 'util' in os.getcwd(): from optimizer import *
+else: from util.optimizer import * 
 
+from sklearn.svm import SVC, SVR  # fit time is quadratic with the n_example (also, one-vs-one multiclass support)
+from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
+from sklearn.linear_model import LogisticRegression, LinearRegression
+from xgboost import XGBClassifier, XGBRegressor
+from sklearn.metrics import roc_auc_score, mean_squared_error, accuracy_score, f1_score
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import learning_curve
 
+import matplotlib.pyplot as plt
+
+def f1(y_true, y_pred):
+    return f1_score(y_true, y_pred, average='weighted')
+    
 class DataWrangler:
     """
         Data ingestion / pre-processing
@@ -44,18 +62,29 @@ class DataWrangler:
         return df.drop(drop_cols, axis=1)
 
     def stack_dfs(self, dfs):
+        ''' Stack train and test data before any transformation '''
         stacked_df = pd.concat(dfs)
         splits = [len(df) for df in dfs[:-1]]
         return stacked_df, splits
 
-    def encode_columns(self, df): 
+    def scale_numeric_columns(self, df): 
         from sklearn.preprocessing import StandardScaler
+        scaler = StandardScaler()
         num_cols = list(df.dtypes[df.dtypes != 'object'].index)
-        df[num_cols] = StandardScaler().fit_transform(df[num_cols])
+        df[num_cols] = scaler.fit_transform(df[num_cols].as_matrix())
+
+        return df, scaler
+
+    def encode_categorical_columns(self, df_train, df_test=None): 
+        if df_test is None:
+            df = df_train
+        else:
+            df = pd.concat([df_train, df_test])
 
         cat_cols = list(df.dtypes[df.dtypes == 'object'].index)
         df = pd.get_dummies(df, columns = obj_cols)
-        return df
+
+        return df[:len(df_train)], df[len(df_train):]
 
     def label_imbalance_check_fix(self, df, target, verbose=False):
         ''' applicable for classfiication problem '''
@@ -94,15 +123,126 @@ class DataWrangler:
                     avg_val = df.mean()[col]
                     df = df.fillna({col: avg_val})
 
-        return df
+    def reduce_dimensions(self, df_train, df_test):
+        from sklearn.decomposition import PCA
+        pca = PCA(.99)
+        x_train = pca.fit_transform(df_train.as_matrix())
+        x_test = pca.transform(df_test.as_matrix())
+        df_train_ = pd.DataFrame(data=x_train,index=df_train.index)
+        df_test_ = pd.DataFrame(data=x_test,index=df_test.index)
+
+        return df_train_, df_test_
+
+lr_params = {"C": [1, 1e3, 1e5], "solver": ['lbfgs']}
+svm_params = {"C": [1000, 10, 1, 1e3]} 
+rf_param_dist = {"max_depth": [None],
+                 "n_estimators": [10, 100, 200, 500, 1000],
+                 "max_features": [None, 0.2, 0.3, 0.5], #None defaults to 'auto'
+                 "min_samples_split": randint(2, 101),
+                 "min_samples_leaf": randint(1, 500),
+                 "bootstrap": [True],
+                 "criterion": ["gini", "entropy"],
+                 "n_jobs": [-1]}
+xgb_param_dist = {"max_depth": randint(3,11),
+                  "learning_rate": [0.2, 0.1, 0.03, 0.01],
+                  "n_estimators": [10, 100, 200, 500, 1000],
+                  "n_jobs": [-1],
+                  "gamma": randint(0,11),
+                  "min_child_weight": randint(1,11),
+                  "subsample": [0.5, 1],
+                  "colsample_bylevel": [0.5, 1],
+                  "reg_lambda": randint(1,100)
+                  }
 
 class Pipeliner:
-    
-    def __init__(self):
-        pass
 
+    def __init__(self):
+        # pre-defined search space
+        self.model_space = []
+        self.model_space.append((LogisticRegression(), lr_params))
+        self.model_space.append((SVC(), svm_params)) 
+        self.model_space.append((RandomForestClassifier(), rf_param_dist))
+        self.model_space.append((XGBClassifier(), xgb_param_dist))  
+
+    def curate_pipeline(self, X, y, budget=50, cv=3, scorer=accuracy_score):
+        #rs = RandomOptimizer(self.model_space, budget=budget)
+        rs_bandit = GreedyOptimizer(self.model_space, budget=budget)
+
+        start = time()
+        #rs = rs.parallel_fit(X, y, cv=cv, scorer=scorer)
+        rs_bandit = rs_bandit.fit(X, y, cv=cv, scorer=scorer)
+        end = time()
+        print ('RandomBanditOptimizer took %s seconds'%(end-start))
+        print ('best score: ', rs.best_score_)
+        print ('using best model: ', rs.best_estimator_)
+
+        return rs.best_estimator_ 
+
+    def random_forest_search(self, X, y, budget=20, cv=3, scoring='f1_weighted'):
+        ''' curate a random forest pipeline '''
+        from sklearn.model_selection import RandomizedSearchCV
+        rs = RandomizedSearchCV( RandomForestClassifier(), rf_param_dist, 
+                                n_iter=budget, cv=cv, n_jobs=-1, scoring=scoring)
+        start = time()
+        rs = rs.fit(X, y)
+        end = time()
+        print ('RandomOptimizer took %s seconds'%(end-start))
+        print ('best score: ', rs.best_score_)
+        print ('using best model: ', rs.best_estimator_)
+
+        return rs.best_estimator_
+
+    def XGB_search(self, X, y, budget=20, cv=3, scoring='f1_weighted'):
+        pass
 
 class VisualAnalyzer:
 
     def __init__(self):
         pass
+
+    def plot_learning_curve(self, model, X, y, cv=5, 
+                            train_sizes=np.linspace(.1,1.0,5),
+                            title=''):
+        train_sizes, train_scores, test_scores = learning_curve(
+            model, X, y, cv=cv, n_jobs=-1, train_sizes=train_sizes)
+        train_scores_mean = np.mean(train_scores, axis=1)
+        train_scores_std = np.std(train_scores, axis=1)
+        test_scores_mean = np.mean(test_scores, axis=1)
+        test_scores_std = np.std(test_scores, axis=1)
+
+        
+        plt.figure()
+        plt.title(title)
+        plt.xlabel("Training examples")
+        plt.ylabel("Score")
+        plt.grid()
+        plt.fill_between(train_sizes, train_scores_mean - train_scores_std,
+                     train_scores_mean + train_scores_std, alpha=0.1,
+                     color="r")
+        plt.fill_between(train_sizes, test_scores_mean - test_scores_std,
+                     test_scores_mean + test_scores_std, alpha=0.1, color="g")
+        plt.plot(train_sizes, train_scores_mean, 'o-', color="r",
+             label="Training score")
+        plt.plot(train_sizes, test_scores_mean, 'o-', color="g",
+             label="Cross-validation score")
+        plt.legend(loc="best")
+
+        return plt
+        
+    def plt_confusion_matrix(self, y_test, y_preds):
+        from sklearn.metrics import confusion_matrix
+        cm = confusion_matrix(y_test, y_preds)
+
+        return cm
+
+
+if __name__ == '__main__':
+    #from sklearn.datasets import fetch_mldata
+    #mnist = fetch_mldata('MNIST original')
+    from sklearn.datasets import load_digits
+    mnist = load_digits()
+
+    x_train, x_test, y_train, y_test = train_test_split(mnist.data, mnist.target, test_size=0.2)
+    tuner = Pipeliner()
+    model = tuner.curate_pipeline(x_train, y_train)
+    print(model.score(x_test, y_test))
---
+++ util/optimizer.py
@@ -0,0 +1,161 @@
+"""
+    Hyperparameter tuning optimizer
+    author: yeounoh chung (yeounohster@gmail.com)
+    data: 4/11/18
+"""
+import numpy as np
+import pandas as pd
+from time import time
+from copy import deepcopy
+import concurrent.futures
+import random
+import math
+import scipy.stats
+from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.model_selection import ShuffleSplit
+from sklearn.metrics import f1_score
+from abc import ABC, abstractmethod
+
+class RandomSearch:
+    def __init__(self, model, param, n_iter):
+        self.best_estimator_ = None
+        self.best_score_ = 0.
+        self.best_param_ = None
+        self.model_ = model
+        self.param_ = param
+        self.n_iter_ = n_iter
+        self.X_v = None
+        self.y_v = None
+
+    def fit(self, X, y, cv=3, scorer=f1_score):
+
+        for i in range(self.n_iter_):
+            param_args = {}
+            for k, p in self.param_.items():
+                if type(p) is list:
+                    param_args[k] = p[random.randint(0,len(p)-1)]
+                elif type(p) is scipy.stats.distributions.rv_frozen:
+                    param_args[k] = p.rvs()
+            self.model_.set_params(**param_args)
+
+            scores = []
+            if cv == 1:
+                x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+                self.model_.fit(x_train,y_train)
+                y_pred = self.model_.predict(x_test)
+                scores.append(scorer(y_test, y_pred))
+            else:
+                kf = ShuffleSplit(n_splits=cv, test_size=0.2)
+                for train, test in kf.split(X):
+                    self.model_.fit(X[train], y[train])
+                    y_pred = self.model_.predict(X[test])
+                    scores.append(scorer(y[test], y_pred))
+            score_ = np.mean(scores) 
+
+            if score_ > self.best_score_:
+                self.best_estimator_ = deepcopy(self.model_)
+                self.best_score_ = score_
+                self.best_param_ = param_args
+
+        return self
+
+class MultiArmedBandit(ABC):
+    def __init__(self, model_space, history=[], rewards={}, budget=25):
+        self.model_space_ = model_space
+        self.history_ = history
+        self.rewards_ = rewards
+        self.budget_ = budget
+
+        self.n_candidates_ = 0
+        self.best_score_ = 0.
+        self.best_estimator_ = None
+
+    @abstractmethod
+    def fit(self, X, y):
+        pass
+
+class RandomOptimizer(MultiArmedBandit):
+    def __init__(self, model_space, history=[], budget=25):
+        super(RandomOptimizer, self).__init__(model_space, history, budget=budget)
+
+    def fit(self, X, y, cv=3, scorer=f1_score):
+        n_arms = len(self.model_space_)
+        for i in range(self.budget_):
+            # exploration
+            target = self.model_space_[random.randint(0, n_arms-1)]
+            rs = RandomSearch(target[0], target[1], 1)
+            rs.fit(X, y, cv=cv, scorer=scorer)
+            self.history_.append((rs.best_score_, rs.best_estimator_))
+            self.n_candidates_ += 1
+        self.history_.sort(key=lambda t:t[0])
+
+        self.best_score_ = self.history_[-1][0]
+        self.best_estimator_= self.history_[-1][1]
+
+        return self
+
+    def parallel_fit(self, X, y, cv=3, scorer=f1_score):
+        n_arms = len(self.model_space_)
+        with concurrent.futures.ProcessPoolExecutor() as executor:
+            batch_jobs = {}
+
+            for i in range(self.budget_):
+                # exploration
+                target = self.model_space_[random.randint(0, n_arms-1)]
+                rs = RandomSearch(target[0], target[1], 1)
+                batch_jobs[executor.submit(rs.fit, X, y, cv, scorer)] = i
+
+            for job in concurrent.futures.as_completed(batch_jobs):
+                if job.cancelled():
+                    print('%s-th job cancelled'%batch_jobs[job])
+                elif job.done():
+                    tag_ = batch_jobs[job]
+                    error = job.exception()
+                    if error:
+                        print('%s error returned: %s'%(tag_, error))
+                    else:
+                        result_ = job.result()
+                        self.history_.append((result_.best_score_, result_.best_estimator_))
+                        self.n_candidates_ += 1
+        self.history_.sort(key=lambda t:t[0])
+
+        if len(self.history_) > 0:
+            self.best_score_ = self.history_[-1][0]
+            self.best_estimator_= self.history_[-1][1]
+
+        return self
+
+class GreedyOptimizer(MultiArmedBandit):
+    def __init__(self, model_space, history=[], rewards={}, budget=25, eps=0.5):
+        super(GreedyOptimizer, self).__init__(model_space, history, rewards, budget)
+        self.eps_ = eps
+
+    def fit(self, X, y, cv=3, scorer=f1_score):
+        n_arms = len(self.model_space_)
+        for i in range(self.budget_):
+            if len(self.history_) == 0 or random.random() < self.eps_:
+                # exploration
+                target_idx = random.randint(0, n_arms-1)
+            else:
+                # exploitation 
+                k = list(self.rewards_.keys())
+                v = [v_[1]/v_[0] for v_ in self.rewards_.values()]
+                target_idx = k[v.index(max(v))]
+            target = self.model_space_[target_idx]
+            rs = RandomSearch(target[0], target[1], 1)
+            rs.fit(X, y, cv=cv, scorer=scorer)
+            self.history_.append((rs.best_score_, rs.best_estimator_))
+
+            if target_idx in self.rewards_:
+                self.rewards_[target_idx] = (self.rewards_[target_idx][0]+1.,
+                                             self.rewards_[target_idx][1]+rs.best_score_)
+            else:
+                self.rewards_[target_idx] = (1., rs.best_score_)
+            self.n_candidates_ += 1
+        self.history_.sort(key=lambda t:t[0])
+
+        self.best_score_ = self.history_[-1][0]
+        self.best_estimator_= self.history_[-1][1]
+
+        return self
+
